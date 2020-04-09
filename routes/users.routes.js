217e41865525910c5feef20b662cd3b124c8bc20
Łukasz Dawidowicz <lddev@outlook.com>
2020-04-09 16:06:23 +0200

    add user post rout

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator'); //validation for post
 
 /* 
-GET api/user
-test route
+POST api/user
+register user
 poublic
+
+check() - will check in name field whne pushing to db. If empty message will pops. tis need also .not().isEmpty()
  */
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    // validation
+    check('name', 'Name is requierd').not().isEmpty(),
+    check('email', 'Add valid email').isEmail(),
+    check('password', 'Password requier min 6 or more characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 /* connect db */
 connectDB();
 
+/* init middleware */
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 /* Define routes */
