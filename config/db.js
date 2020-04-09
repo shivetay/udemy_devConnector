ab23ebc4d8b7d7fee5da3641dd6fa05fbfc0bd6d
Łukasz Dawidowicz <lddev@outlook.com>
2020-04-09 09:55:53 +0200

    add mongoDB connection

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+/*
+mongoose.connect(db) returns promise
+ */
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB running O_O');
+  } catch (err) {
+    console.error(err.message);
+    //after catching error this will exit whole process
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
