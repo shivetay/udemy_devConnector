5bd7ef5c37330a29c5d3224c805630a1b87341c6
Łukasz Dawidowicz <lddev@outlook.com>
2020-04-15 09:39:22 +0200

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>App.js</h1>
-    </div>
-  );
+import MainLayout from '../src/components/layout/MainLayout/MainLayout';
+import Landing from '../src/components/views/Landing/Landing';
+import Login from '../src/components/features/Login/Login';
+import Register from '../src/components/features/Register/Register';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <MainLayout>
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </MainLayout>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Header from '../Header/Header';
+
+import '../../../styles/global.scss';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+export default MainLayout;
