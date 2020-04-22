b5d8f392adb06761a4f57405ab6f2ff31a76ad1c
Łukasz Dawidowicz <lddev@outlook.com>
2020-04-22 10:31:28 +0200

    predeploy prep

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
@@ -14,7 +15,7 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 /* Define routes */
 
@@ -22,6 +23,16 @@ app.use('/api/users', require('./routes/users.routes'));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/profile', require('./routes/profiles.routes'));
 app.use('/api/post', require('./routes/posts.routes'));
+
+//serve static assets in prod
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.reslove(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 
