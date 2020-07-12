833fadf639dd6485cb58329596dce77f66f06d28
Łukasz Dawidowicz <lddev@outlook.com>
2020-07-12 11:10:53 +0200

    changes

---
+++ client/build/precache-manifest.41e3c9184550194fdda63b1a8589f204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f782dca3eb6be9b5cc0e2e03941a26a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "893963793c8ddf5baf28",
+    "url": "/static/css/main.8c883ac5.chunk.css"
+  },
+  {
+    "revision": "220fe82f321806d36333",
+    "url": "/static/js/2.5e1730fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5e1730fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893963793c8ddf5baf28",
+    "url": "/static/js/main.d24c5634.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/client/build')));
+// app.use(express.static(path.join(__dirname, '/client/build')));
 
 // app.get('/', (req, res) => res.send('API Running'));
 
@@ -29,14 +29,18 @@ app.use('/api/post', require('./routes/posts.routes'));
 
 //serve static assets in prod
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// });
 
-// if (process.env.NODE_ENV === 'production') {
-//   //set static folder
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('/client/build'));
 
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 
